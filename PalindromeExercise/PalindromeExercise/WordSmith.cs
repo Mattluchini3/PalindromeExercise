cb4b7180955b41a436ff8b19386bbaff875f4bdb
Matthew Luchini <luch@Matthews-MBP.lan>
2023-03-02 14:55:52 -0500

    Palindrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+
+		}
+		public bool IsAPalindrome(string word)
+		{
+			var reverse = "";
+			for (int i = word.Length - 1; i >= 0; i--) 
+			{
+				reverse += word[i];
+			}
+			if (reverse == word)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+	}
+
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("mom",true)]
+        [InlineData("wasabi", false)]
+        public void Test1(string word, bool expected)
         {
-
+            var myTest = new WordSmith();
+            var actual = myTest.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
